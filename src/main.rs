6d697dc96748d03c1318a9c50ab277834e37da0d
Xientra <articbuisness@gmail.com>
2022-12-30 13:58:02 +0000

    feat: uwuify input text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
+fn uwuify(text: &String) -> String {
+    let replacements = HashMap::from([
+        ("w", "w-w"),
+        ("l", "w"),
+        ("r", "w"),
+        ("n", "ny"),
+        ("f", "th"),
+        (".", "!!!"),
+        ("ove", "uv")
+    ]);
+    let mut result: String = text.clone();
+
+    for (key, value) in replacements {
+        result = result.replace(key, value);
+        result = result.replace(&key.to_uppercase(), &value.to_uppercase());
+    }
+
+    return result;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let input = &mut String::new();
+
+    #[allow(unused_must_use)]
+    loop {
+        input.clear();
+        stdin.read_line(input);
+        println!("{}", uwuify(input));
+    }
 }
